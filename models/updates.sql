fa169e6cd44ef5002b12564813d275920304f1d4
pborissow <email@pborissow.dev>
2025-02-26 09:53:25 -0500

    Fixed typo

--- models/updates.sql
+++ models/updates.sql
@@ -1,12 +1,12 @@
 -- Index Fields for Search/Ingest
-CREATE INDEX IDX_PLACE_SOURCE_KEY ON GAZETTER.PLACE(SOURCE_KEY);
-CREATE INDEX IDX_PLACE_COUNTRY_CODE ON GAZETTER.PLACE(COUNTRY_CODE);
-CREATE INDEX IDX_PLACE_RANK ON GAZETTER.PLACE(RANK);
-CREATE INDEX IDX_PLACE_TYPE ON GAZETTER.PLACE(TYPE);
-CREATE INDEX IDX_PLACE_SUBTYPE ON GAZETTER.PLACE(SUBTYPE);
+CREATE INDEX IDX_PLACE_SOURCE_KEY ON GAZETTEER.PLACE(SOURCE_KEY);
+CREATE INDEX IDX_PLACE_COUNTRY_CODE ON GAZETTEER.PLACE(COUNTRY_CODE);
+CREATE INDEX IDX_PLACE_RANK ON GAZETTEER.PLACE(RANK);
+CREATE INDEX IDX_PLACE_TYPE ON GAZETTEER.PLACE(TYPE);
+CREATE INDEX IDX_PLACE_SUBTYPE ON GAZETTEER.PLACE(SUBTYPE);
 
 
 
 -- Create unique index on the place table
-create unique index idx_place_unique on gazetter.place(source_id, source_key, country_code);
-create unique index idx_name_unique on gazetter.name(place_id, uname);
+create unique index idx_place_unique on gazetteer.place(source_id, source_key, country_code);
+create unique index idx_name_unique on gazetteer.name(place_id, uname);
