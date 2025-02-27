9c56242e518cbbe0bd8e28bb9d60e61d30a9ba42
pborissow <email@pborissow.dev>
2025-02-27 06:23:53 -0500

    Updated Counter to use the StatusLogger from javaxt-express

--- src/com/kartographia/gazetteer/utils/Counter.java
+++ src/com/kartographia/gazetteer/utils/Counter.java
@@ -1,19 +1,20 @@
 package com.kartographia.gazetteer.utils;
-import static com.kartographia.gazetteer.utils.StringUtils.*;
+
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
-  /** Used to render status updates to the console.
-   */
+
+import javaxt.express.utils.StatusLogger;
+import static javaxt.express.utils.StringUtils.*;
+
 public class Counter {
-    private String statusText = "Status: 0  %  ETC: ---------- --:-- --";
-    private int percentComplete = 0;
-    private long ttl;
-    private AtomicLong counter;
-    private long startTime;
+
+    private AtomicLong recordCounter;
+    private StatusLogger statusLogger;
+
 
   //Instantiate the counter with a known record count
     public Counter(long ttl){
-        this.ttl = ttl;
-        init();
+        init(ttl);
     }
 
     public Counter(javaxt.io.File file, boolean skipHeader) throws Exception {
@@ -24,83 +25,38 @@ public class Counter {
     public Counter(java.io.BufferedReader br, boolean skipHeader) throws Exception {
         System.out.print("Analyzing File...");
         long t = System.currentTimeMillis();
-        countRows(br, skipHeader);
+        long ttl = countRows(br, skipHeader);
         String elapsedTime = getElapsedTime(t);
         System.out.println(" Done!");
         System.out.println("Found " + format(ttl) + " records in " + elapsedTime);
-        init();
+        init(ttl);
     }
 
-    private void countRows(java.io.BufferedReader br, boolean skipHeader) throws Exception{
-        ttl = 0L;
+    private long countRows(java.io.BufferedReader br, boolean skipHeader) throws Exception{
+        long ttl = 0L;
         if (skipHeader) br.readLine();
         while (br.readLine()!=null){
             ttl++;
         }
         br.close();
-    }
-
-    public long getTotal(){
         return ttl;
     }
 
-    private void init(){
-        javaxt.utils.Date startDate = new javaxt.utils.Date();
-        startDate.setTimeZone("America/New York");
-        System.out.println("Starting ingest at " + startDate.toString("yyyy-MM-dd HH:mm a"));
-        startTime = System.currentTimeMillis();
-        counter = new AtomicLong(0);
+    public long getTotal(){
+        return statusLogger.getTotalRecords();
+    }
+
+    private void init(long ttl){
+        recordCounter = new AtomicLong(0);
+        statusLogger = new StatusLogger(recordCounter);
+        statusLogger.setTotalRecords(ttl);
     }
 
     public void updateCount(){
-        if (counter.get()==0) System.out.print(statusText);
-        long x = counter.incrementAndGet();
-        double p = ((double) x/ (double) ttl);
-        int currPercent = (int) Math.round(p*100);
-        if (currPercent > percentComplete){
-            percentComplete = currPercent;
-
-
-            long currTime = System.currentTimeMillis();
-            int elapsedTime = (int) Math.round(((currTime-startTime)/1000)/60); //minutes
-            int totalTime = (int) Math.round((double)elapsedTime/p); //minutes
-            int timeRemaining = totalTime - elapsedTime;
-
-            javaxt.utils.Date etc = new javaxt.utils.Date();
-            etc.add(timeRemaining, "minutes");
-
-
-            if (percentComplete==100) etc = new javaxt.utils.Date();
-            etc.setTimeZone("America/New York");
-
-            String _etc = etc.toString("yyyy-MM-dd HH:mm a");
-            if (elapsedTime==0) _etc = "---------- --:-- --";
-
-
-            for (int i=0; i<statusText.length(); i++){
-                System.out.print("\b");
-            }
-            String str = statusText.replace("0  %", pad(percentComplete)+"%");
-            str = str.replace("---------- --:-- --", _etc);
-
-            System.out.print(str);
-        }
+        recordCounter.incrementAndGet();
     }
 
-  //**************************************************************************
-  //** pad
-  //**************************************************************************
-  /** Used to pad a number with white spaces. Used when printing percent
-   *  complete to the standard output stream.
-   */
-    private static String pad(int i){
-        String s = ""+i;
-        if(s.length()==1){
-          s += "  ";
-        }
-        else if(s.length()==2){
-          s += " ";
-        }
-        return s;
+    public void stop(){
+        statusLogger.shutdown();
     }
 }
