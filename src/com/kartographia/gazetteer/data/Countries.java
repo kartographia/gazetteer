36d94490e5926bbc445d9fd20d224589d8ae63f4
pborissow <email@pborissow.dev>
2025-02-28 13:49:28 -0500

    Updated the Countries class to parse the new countries.csv file

--- src/com/kartographia/gazetteer/data/Countries.java
+++ src/com/kartographia/gazetteer/data/Countries.java
@@ -11,10 +11,10 @@ import javaxt.express.utils.CSV;
  *
  ******************************************************************************/
 
-
 public class Countries {
 
-    private HashMap<String, javaxt.utils.Record> records = new HashMap<>();
+    private HashMap<String, javaxt.utils.Record> records;
+    private HashMap<String, String> fipsToISO;
 
 
   //**************************************************************************
@@ -24,29 +24,85 @@ public class Countries {
    *  like one found in the data directory (e.g. "countries.csv")
    */
     public Countries(javaxt.io.File countries) throws Exception {
-
+        records = new HashMap<>();
+        fipsToISO = new HashMap<>();
 
         try(java.io.InputStream is = countries.getInputStream()){
             String header = CSV.readLine(is); //skip header
+            //boolean v1 = header.startsWith("CC,Name,");
             String row;
             while (!(row=CSV.readLine(is)).isEmpty()){
                 CSV.Columns columns = CSV.getColumns(row, ",");
-                String fips = columns.get(0).toString();
-                String name = columns.get(1).toString();
-                String iso = columns.get(2).toString();
-                String region = columns.get(3).toString();
+                String iso2 = columns.get(0).toString();
+                String iso3 = columns.get(1).toString();
+                String fips = columns.get(2).toString();
+                String name = columns.get(3).toString();
+                String admin = columns.get(4).toString();
+                String region = columns.get(5).toString();
                 javaxt.utils.Record record = new javaxt.utils.Record();
                 record.set("name", name);
                 record.set("fips", fips);
-                record.set("iso", iso);
+                record.set("iso2", iso2);
+                record.set("iso3", iso3);
                 record.set("region", region);
-                records.put(fips, record);
+                fipsToISO.put(fips, iso2);
+                records.put(iso2, record);
             }
         }
     }
 
-    public javaxt.utils.Record getCountry(String fips){
-        return records.get(fips);
+
+  //**************************************************************************
+  //** getFIPSCode
+  //**************************************************************************
+  /** Returns a FIPS country code for a given ISO code
+   *  @param iso 2 character ISO code
+   */
+    public String getFIPSCode(String iso){
+        return records.get(iso).get("fips").toString();
     }
 
+
+  //**************************************************************************
+  //** getISOCode
+  //**************************************************************************
+  /** Returns a 2 character ISO country code for a given FIPS code
+   *  @param fips 2 character FIPS code
+   */
+    public String getISOCode(String fips){
+        return fipsToISO.get(fips);
+    }
+
+
+  //**************************************************************************
+  //** getCountryByFIPSCode
+  //**************************************************************************
+  /** Returns a country record for a given FIPS code
+   *  @param fips 2 character FIPS code
+   */
+    public javaxt.utils.Record getCountryByFIPSCode(String fips){
+        return records.get(fipsToISO.get(fips));
+    }
+
+
+  //**************************************************************************
+  //** getCountryByISOCode
+  //**************************************************************************
+  /** Returns a country record for a given ISO code
+   *  @param iso 2 character ISO code
+   */
+    public javaxt.utils.Record getCountryByISOCode(String iso){
+        return records.get(iso);
+    }
+
+
+  //**************************************************************************
+  //** getISO3
+  //**************************************************************************
+  /** Converts a 2 character ISO code to a 3 character code
+   *  @param iso2 2 character ISO code
+   */
+    public String getISO3(String iso2){
+        return records.get(iso2).get("iso3").toString();
+    }
 }
